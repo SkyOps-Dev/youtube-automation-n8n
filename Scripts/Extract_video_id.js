372b1dd50b619b2742cc47c0a738188eeb857387
Imran Latif <imranlatifstar2015@gmail.com>
2025-08-19 11:14:52 +0500

    Update Extract_video_id.js

--- Scripts/Extract_video_id.js
+++ Scripts/Extract_video_id.js
@@ -1,1 +1,11 @@
-
+return items.map(item => {
+  const videoLink = item.json["Video Link"];
+  const match = videoLink.match(/(?:v=|\/)([0-9A-Za-z_-]{11})/);
+  const videoId = match ? match[1] : null;
+  return {
+    json: {
+      "Video Link": videoLink,
+      "video_id": videoId
+    }
+  };
+});
